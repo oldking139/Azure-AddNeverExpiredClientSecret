5fda6f9a28243e8f2b183224599498aed14204a4
oldking139 <45203648+oldking139@users.noreply.github.com>
2021-08-18 18:08:56 +0800

    Add files via upload

---
+++ Add-client-secret.ps1
@@ -0,0 +1,16 @@
+Write-Output "感谢使用AZURE应用Client secret过期时间定制脚本"
+$Get_AzTenant_Id = Read-Host "请输入你需要注册的应用的账户的租户id（Tenant id）"
+if ([string]::IsNullOrWhiteSpace($Get_AzTenant_Id))
+{
+$Get_AzTenant_Id == Get-AzTenant
+}
+
+$startDate = Get-Date
+$AddYears = Read-Host "请输入您注册client secret所需要的过期时间(默认过期时间为300年)"
+if ([string]::IsNullOrWhiteSpace($AddYears))
+{$AddYears = ‘300’}
+
+$endDate = $startDate.AddYears($AddYears)
+$ObjectId = Read-Host "请输入您所需要注册client secret的应用（Application）的ObjectId"
+$aadAppsecret01 = New-AzureADApplicationPasswordCredential -ObjectId $ObjectId -StartDate $startDate -EndDate $endDate
+Write-Output $aadAppsecret01
