e0a995a966009e0560f86bf9f95292179be56f52
oldking139 <45203648+oldking139@users.noreply.github.com>
2021-08-19 22:58:03 +0800

    Update Add-client-secret.ps1

--- Add-client-secret.ps1
+++ Add-client-secret.ps1
@@ -3,8 +3,6 @@ Write-Output "感谢使用AZURE应用Client secret过期时间定制脚本"
 $Get_AzTenant_Id = Read-Host "请输入注册应用所属的账户的租户id（留空为当前用户）"
 if ([string]::IsNullOrWhiteSpace($Get_AzTenant_Id))
 {$Get_AzTenant_Id = Get-AzTenant}
-
-Write-Output "应用租户id"
 Connect-AzureAD -TenantId $Get_AzTenant_Id -confirm
 
 $startDate = Get-Date
